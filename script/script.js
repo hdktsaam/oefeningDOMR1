cb174375d3eff6cedf69ebb3e73ca2bebb7d344e
hdktsaam <hannes.dekesel@tsaam.be>
2025-05-02 15:33:54 +0200

    start

---
+++ script/script.js
@@ -0,0 +1,22 @@
+const groenten = [
+  {
+    id: 1,
+    naam: "prei",
+    prijs: 2.1,
+  },
+  {
+    id: 1,
+    naam: "brocoli",
+    prijs: 1.5,
+  },
+  {
+    id: 1,
+    naam: "tomaat",
+    prijs: 3.4,
+  },
+  {
+    id: 1,
+    naam: "erwten",
+    prijs: 0.7,
+  },
+];
